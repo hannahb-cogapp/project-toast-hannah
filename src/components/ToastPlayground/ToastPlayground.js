defb656ec60f9eb54c074a03c86495676748a093
Hannah Baker <hannahb@cogapp.com>
2024-08-14 14:52:46 +0100

    Complete exercise two

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Button from '../Button';
+import Toast from '../Toast';
 
 import styles from './ToastPlayground.module.css';
 
@@ -9,6 +10,7 @@ const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
 function ToastPlayground() {
   const [message, setMessage] = React.useState('');
   const [variantSelected, setVariantSelected] = React.useState(VARIANT_OPTIONS[0]);
+  const [showToast, setShowToast] = React.useState(false);
 
   return (
     <div className={styles.wrapper}>
@@ -16,6 +18,9 @@ function ToastPlayground() {
         <img alt="Cute toast mascot" src="/toast.png" />
         <h1>Toast Playground</h1>
       </header>
+      { showToast &&
+        <Toast message={message} variant={variantSelected} setShowToast={setShowToast}/>
+      }
 
       <div className={styles.controlsWrapper}>
         <div className={styles.row}>
@@ -72,7 +77,13 @@ function ToastPlayground() {
           <div
             className={`${styles.inputWrapper} ${styles.radioWrapper}`}
           >
-            <Button>Pop Toast!</Button>
+            <Button       
+              onClick={() => {
+                setShowToast(true);
+              }}
+            >
+                  Pop Toast!
+            </Button>
           </div>
         </div>
       </div>
